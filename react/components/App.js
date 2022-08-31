d759b4fa46b3f30265d938f3c45ec0bae0ae5e87
markluke10 <marklwharmby@hotmail.com>
2022-08-31 10:09:27 +0100

    get requests

---
+++ react/components/App.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const {db} = require('../db')
+const app = express()
+const {Items} = require('../models')
+const {itemsRt} = require('../route')
+
+
+app.use(express.json())
+app.use('/items, itemsRt')
+
