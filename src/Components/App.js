e718a578a4324b44818c7c458f7280c3ff54ac2a
Chrisd313 <chrisd313@hotmail.com>
2022-08-31 14:44:52 +0100

    chris

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import {Switch, Route, Routes, BrowserRouter, Redirect, withRouter, NavLink, Link} from 'react-router-dom';
+import Navbar from './Navbar';
+
+import '..';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HELLO WORLD!</h1>
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return <nav className="nav">
+        <a href="/" className="site-title">SITE NAME</a>
+        <ul>
+            <li>
+                <a href="/home">Home</a>
+                <a href="/create item">Create item</a>
+            </li>
+        </ul>
+    </nav>
+}
