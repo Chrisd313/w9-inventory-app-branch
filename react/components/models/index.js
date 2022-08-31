6b776281256b53c1a070ef0b780adb82ab2f38e8
Chrisd313 <chrisd313@hotmail.com>
2022-08-31 14:21:58 +0100

    Components

--- react/components/models/index.js
+++
@@ -1,7 +0,0 @@
-const {user} = require('./user')
-const {items} = require('./items')
-
-items.belongsTo(user)
-user.hasMany(items)
-
-module.exports = {items, user};
--- react/components/models/user.js
+++
@@ -1,8 +0,0 @@
-const {DataTypes, db} = require('./db') /* this might need to change depending on file path */
-
-const user = db.define('user', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-});
-
-module.exports = {user};
